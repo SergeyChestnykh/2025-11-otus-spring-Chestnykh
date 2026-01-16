a2d213d466183492dce8289190ce0cef1509bd7e
user <my@email.com>
2026-01-16 13:12:43 +0300

    Feat: init hw04

---
+++ hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -0,0 +1,30 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+@Service
+@RequiredArgsConstructor
+public class TestRunnerServiceImpl implements TestRunnerService {
+
+    private final LocalizedIOService ioService;
+
+    private final TestService testService;
+
+    private final StudentService studentService;
+
+    private final ResultService resultService;
+
+    @Override
+    public void run() {
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException e) {
+            ioService.printLineLocalized("TestRunnerService.unable.read.questions");
+        }
+    }
+}
+

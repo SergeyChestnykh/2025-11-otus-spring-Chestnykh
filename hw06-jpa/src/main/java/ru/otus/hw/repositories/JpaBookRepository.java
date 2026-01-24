835d8d815a7fe34bbed8588403ad9d62dc5a9fb5
user <my@email.com>
2026-01-24 20:17:15 +0300

    Feat: Book Jpa

---
+++ hw06-jpa/src/main/java/ru/otus/hw/repositories/JpaBookRepository.java
@@ -0,0 +1,54 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class JpaBookRepository implements BookRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+
+    @Override
+    public Optional<Book> findById(long id) {
+        var query = em.createQuery(
+                "select b from Book b " +
+                        "join fetch b.author " +
+                        "where b.id = :id",
+                Book.class
+        );
+        query.setParameter("id", id);
+
+        try {
+            return Optional.of(query.getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return em.createQuery("select b from Book b join fetch b.author", Book.class).getResultList();
+    }
+
+    @Override
+    public Book save(Book book) {
+        return em.merge(book);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        Query query = em.createQuery("delete from Book b where b.id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+}

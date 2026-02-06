1c3e612efb13f7b4ca9a33575e91338009acb0ac
user <my@email.com>
2026-02-06 21:52:45 +0300

    Feat: comments crud

--- hw06-jpa/src/main/java/ru/otus/hw/commands/CommentCommands.java
+++ hw06-jpa/src/main/java/ru/otus/hw/commands/CommentCommands.java
@@ -12,7 +12,6 @@ import java.util.stream.Collectors;
 @ShellComponent
 public class CommentCommands {
     private final CommentService commentService;
-
     private final CommentConverter commentConverter;
 
     @ShellMethod(value = "Find all comments for book", key = "bc")
@@ -20,5 +19,29 @@ public class CommentCommands {
         return commentService.findAllForBook(bookId).stream()
                 .map(commentConverter::commentToString)
                 .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    @ShellMethod(value = "Find comment by id", key = "ccid")
+    public String findCommentById(long id) {
+        return commentService.find(id)
+                .map(commentConverter::commentToString)
+                .orElse("Comment with id %d not found".formatted(id));
+    }
+
+    @ShellMethod(value = "Insert comment", key = "cins")
+    public String insertComment(long bookId, String text) {
+        var savedComment = commentService.insert(bookId, text);
+        return commentConverter.commentToString(savedComment);
+    }
+
+    @ShellMethod(value = "Update comment", key = "cupd")
+    public String updateComment(long id, String text) {
+        var savedComment = commentService.update(id, text);
+        return commentConverter.commentToString(savedComment);
+    }
+
+    @ShellMethod(value = "Delete comment by id", key = "cdel")
+    public void deleteComment(long id) {
+        commentService.deleteById(id);
     }
 }

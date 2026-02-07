69ebd419fb3ecfe144b5aedfec7fb1a05f1add45
user <my@email.com>
2026-02-07 15:05:22 +0300

    Fix: repositories annotations

--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -2,6 +2,7 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Override
     @EntityGraph(attributePaths = {"author", "genres"})
+    @NonNull
     List<Book> findAll();
 }

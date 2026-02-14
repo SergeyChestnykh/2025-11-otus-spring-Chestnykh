d85891912539ce3084a2d74b1081538430003734
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-14 20:21:56 +0300

    Fix: some bugs

--- hw08-mongo/src/main/java/ru/otus/hw/services/AuthorServiceImpl.java
+++ hw08-mongo/src/main/java/ru/otus/hw/services/AuthorServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.converters.AuthorConverter;
 import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.repositories.AuthorRepository;
@@ -16,6 +17,7 @@ public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorConverter authorConverter;
 
+    @Transactional(readOnly = true)
     @Override
     public List<AuthorDto> findAll() {
         return authorRepository.findAll().stream().map(authorConverter::authorToDto).collect(Collectors.toList());
--- hw08-mongo/src/main/java/ru/otus/hw/services/GenreServiceImpl.java
+++ hw08-mongo/src/main/java/ru/otus/hw/services/GenreServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.converters.GenreConverter;
 import ru.otus.hw.dto.GenreDto;
 import ru.otus.hw.repositories.GenreRepository;
@@ -18,6 +19,7 @@ public class GenreServiceImpl implements GenreService {
     private final GenreConverter genreConverter;
 
     @Override
+    @Transactional(readOnly = true)
     public List<GenreDto> findAll() {
         return genreRepository.findAll().stream().map(genreConverter::genreToDto).collect(Collectors.toList());
     }

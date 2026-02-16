2818fc4133d6249c2d2c85b7d4fae611e4441b3b
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-16 21:39:54 +0300

    Fix: 1

--- hw07-data-jpa/src/main/java/ru/otus/hw/Application.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/Application.java
@@ -2,10 +2,8 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("ru.otus.hw.models")
 public class Application {
 
     public static void main(String[] args) {
--- hw07-data-jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07-data-jpa/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Override
-    @EntityGraph(attributePaths = {"author", "genres"})
+    @EntityGraph(attributePaths = {"author"})
     @NonNull
     List<Book> findAll();
 

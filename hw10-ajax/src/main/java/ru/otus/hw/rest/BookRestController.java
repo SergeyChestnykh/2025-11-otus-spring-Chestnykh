d32f5adcd197fbb4542c5746ec51ae82d5d6527d
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-22 19:39:20 +0300

    Feat: edit rest

--- hw10-ajax/src/main/java/ru/otus/hw/rest/BookRestController.java
+++ hw10-ajax/src/main/java/ru/otus/hw/rest/BookRestController.java
@@ -1,19 +1,32 @@
 package ru.otus.hw.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.GenreDto;
+import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.GenreService;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class BookRestController {
     private final BookService bookService;
+    private final AuthorService authorService;
+    private final GenreService genreService;
 
     @GetMapping("/api/book")
     List<BookDto> getAll() {
@@ -28,5 +41,39 @@ public class BookRestController {
     @DeleteMapping("/api/book/{id}")
     void deleteById(@PathVariable long id) {
         bookService.deleteById(id);
+    }
+
+    @GetMapping("/api/authors")
+    List<AuthorDto> getAllAuthors() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/api/genres")
+    List<GenreDto> getAllGenres() {
+        return genreService.findAll();
+    }
+
+    @PostMapping("/api/book")
+    ResponseEntity<BookDto> createBook(@RequestBody Map<String, Object> bookData) {
+        String title = (String) bookData.get("title");
+        long authorId = ((Number) bookData.get("authorId")).longValue();
+        List<Long> genreIds = ((List<Number>) bookData.get("genreIds")).stream()
+                .map(Number::longValue)
+                .toList();
+        
+        bookService.insert(title, authorId, new HashSet<>(genreIds));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/api/book/{id}")
+    ResponseEntity<BookDto> updateBook(@PathVariable long id, @RequestBody Map<String, Object> bookData) {
+        String title = (String) bookData.get("title");
+        Long authorId = ((Number) bookData.get("authorId")).longValue();
+        List<Long> genreIds = ((List<Number>) bookData.get("genreIds")).stream()
+                .map(Number::longValue)
+                .toList();
+        
+        bookService.update(id, title, authorId, new HashSet<>(genreIds));
+        return ResponseEntity.ok().build();
     }
 }

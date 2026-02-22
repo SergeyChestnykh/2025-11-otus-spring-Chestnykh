96558501b70db84225219d4c1f69404f3a78a51a
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-22 19:04:25 +0300

    Feat: add comment

--- hw10-ajax/src/main/java/ru/otus/hw/rest/CommentRestController.java
+++ hw10-ajax/src/main/java/ru/otus/hw/rest/CommentRestController.java
@@ -1,9 +1,12 @@
 package ru.otus.hw.rest;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.services.CommentService;
@@ -24,6 +27,11 @@ public class CommentRestController {
     @DeleteMapping("/api/comments/{commentId}")
     public void deleteComment(@PathVariable Long commentId) {
         commentService.deleteById(commentId);
+    }
+
+    @PostMapping("/api/book/{bookId}/comments")
+    public void addComment(@PathVariable Long bookId, @Valid @RequestBody CommentDto commentDto) {
+        commentService.insert(bookId, commentDto.text());
     }
 
 }

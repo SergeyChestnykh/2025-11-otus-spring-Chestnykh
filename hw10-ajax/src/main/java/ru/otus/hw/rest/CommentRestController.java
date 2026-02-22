8f6fd0abda3a9314a9c1df8903f6b9f576f7436b
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-22 18:18:39 +0300

    Feat: detail.html load and delete comment

---
+++ hw10-ajax/src/main/java/ru/otus/hw/rest/CommentRestController.java
@@ -0,0 +1,29 @@
+package ru.otus.hw.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.hw.dto.CommentDto;
+import ru.otus.hw.services.CommentService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentRestController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/api/book/{id}/comments")
+    public List<CommentDto> getAllForBook(@PathVariable Long id) {
+        return commentService.findAllForBook(id);
+    }
+
+    @DeleteMapping("/api/book/{bookId}/comments/{commentId}")
+    public void deleteComment(@PathVariable Long bookId, @PathVariable Long commentId) {
+        commentService.deleteById(commentId);
+    }
+
+}

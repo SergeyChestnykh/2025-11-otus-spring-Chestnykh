52c0513dda487411cd86e454e03ca01fc8b1ee26
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-02-28 13:12:52 +0300

    Feat: authors ajax

--- hw10-ajax/src/main/java/ru/otus/hw/controllers/AuthorController.java
+++ hw10-ajax/src/main/java/ru/otus/hw/controllers/AuthorController.java
@@ -2,19 +2,14 @@ package ru.otus.hw.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.otus.hw.services.AuthorService;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthorController {
 
-    private final AuthorService authorService;
-
     @GetMapping("/authors")
-    public String listAuthors(Model model) {
-        model.addAttribute("authorList", authorService.findAll());
+    public String listAuthors() {
         return "authors";
     }
 }

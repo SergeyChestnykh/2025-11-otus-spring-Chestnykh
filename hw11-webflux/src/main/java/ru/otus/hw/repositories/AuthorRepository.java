6ac5ee2da0b9781f1f4ac628c55fcd9d78bdc471
SergeyChestnykh <sergey.chestnykh@tutamail.com>
2026-03-01 13:45:27 +0300

    Fix: AuthorRepository.java

--- hw11-webflux/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++ hw11-webflux/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -1,9 +1,11 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.lang.NonNull;
 import reactor.core.publisher.Flux;
 import ru.otus.hw.models.Author;
 
 public interface AuthorRepository extends ReactiveCrudRepository<Author, Long> {
+    @NonNull
     Flux<Author> findAll();
 }
